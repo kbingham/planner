96ba4cb93604e87ec652a9c3807abfe076a61b34
Kieran Bingham <kieran@bingham.xyz>
2019-01-13 12:44:54 +0000

    planner: First grid layout

--- planner.py
+++ planner.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
 
-from flask import render_template
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+
+@app.route('/2019')
+def year():
+    current = 2019
+    with app.app_context():
+        return render_template('year.html', \
+            title = "2019", \
+            weekdays = [ 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun' ],
+            months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ],
+        )
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return year()
 
 
 # https://markhneedham.com/blog/2017/04/27/python-flask-generating-a-static-html-page/
 if __name__ == "__main__":
+    app.config['SERVER_NAME'] = "localhost"
     with app.app_context():
-        rendered = render_template('year.html', \
-            title = "2019", \
-            people = [{"name": "Mark"},
-                      {"name": "Michael"}])
-        print(rendered)
-
+        print(year())
